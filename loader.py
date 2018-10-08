1b1993c83948f17651453814293bd6e8f18a5f36
Andrea Censi <acensi@ethz.ch>
2018-10-08 12:11:48 +0200

    fix

--- loader.py
+++ loader.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python2
+import datetime
+import time
+
 
 def duckiebot_loader():
+    print(datetime.datetime.now().isoformat())
     print('duckiebot_loader is empty for now')
+
+    time.sleep(60 * 10)
 
 
 if __name__ == '__main__':
